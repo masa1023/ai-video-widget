800d0ab1c09419c833d1f3b3bd60992c34a13c5d
Masa <masa373.dev@gmail.com>
2025-12-26 18:45:50 +0900

    fix: revert change in aloop navigation

---
+++ packages/widget/src/navigation-graph/degipro.js
@@ -0,0 +1,188 @@
+export const navigationGraph = {
+  'node-1': {
+    videoId: 1,
+    nextNodes: [
+      { id: 'node-2', videoId: 2 },
+      { id: 'node-3', videoId: 3 },
+      { id: 'node-4', videoId: 4 },
+    ],
+  },
+  'node-2': {
+    videoId: 1,
+    nextNodes: [
+      { id: 'node-2', videoId: 2 },
+      { id: 'node-3', videoId: 3 },
+      { id: 'node-4', videoId: 4 },
+    ],
+  },
+  'node-3': {
+    videoId: 1,
+    nextNodes: [
+      { id: 'node-2', videoId: 2 },
+      { id: 'node-3', videoId: 3 },
+      { id: 'node-4', videoId: 4 },
+    ],
+  },
+  'node-4': {
+    videoId: 1,
+    nextNodes: [
+      { id: 'node-2', videoId: 2 },
+      { id: 'node-3', videoId: 3 },
+      { id: 'node-4', videoId: 4 },
+    ],
+  },
+  'node-5': {
+    videoId: 1,
+    nextNodes: [
+      { id: 'node-2', videoId: 2 },
+      { id: 'node-3', videoId: 3 },
+      { id: 'node-4', videoId: 4 },
+    ],
+  },
+  'node-6': {
+    videoId: 1,
+    nextNodes: [
+      { id: 'node-2', videoId: 2 },
+      { id: 'node-3', videoId: 3 },
+      { id: 'node-4', videoId: 4 },
+    ],
+  },
+  'node-7': {
+    videoId: 1,
+    nextNodes: [
+      { id: 'node-2', videoId: 2 },
+      { id: 'node-3', videoId: 3 },
+      { id: 'node-4', videoId: 4 },
+    ],
+  },
+  'node-8': {
+    videoId: 1,
+    nextNodes: [
+      { id: 'node-2', videoId: 2 },
+      { id: 'node-3', videoId: 3 },
+      { id: 'node-4', videoId: 4 },
+    ],
+  },
+  'node-9': {
+    videoId: 1,
+    nextNodes: [
+      { id: 'node-2', videoId: 2 },
+      { id: 'node-3', videoId: 3 },
+      { id: 'node-4', videoId: 4 },
+    ],
+  },
+  'node-10': {
+    videoId: 1,
+    nextNodes: [
+      { id: 'node-2', videoId: 2 },
+      { id: 'node-3', videoId: 3 },
+      { id: 'node-4', videoId: 4 },
+    ],
+  },
+  'node-11': {
+    videoId: 1,
+    nextNodes: [
+      { id: 'node-2', videoId: 2 },
+      { id: 'node-3', videoId: 3 },
+      { id: 'node-4', videoId: 4 },
+    ],
+  },
+  'node-12': {
+    videoId: 1,
+    nextNodes: [
+      { id: 'node-2', videoId: 2 },
+      { id: 'node-3', videoId: 3 },
+      { id: 'node-4', videoId: 4 },
+    ],
+  },
+  'node-13': {
+    videoId: 1,
+    nextNodes: [
+      { id: 'node-2', videoId: 2 },
+      { id: 'node-3', videoId: 3 },
+      { id: 'node-4', videoId: 4 },
+    ],
+  },
+  'node-14': {
+    videoId: 1,
+    nextNodes: [
+      { id: 'node-2', videoId: 2 },
+      { id: 'node-3', videoId: 3 },
+      { id: 'node-4', videoId: 4 },
+    ],
+  },
+  'node-15': {
+    videoId: 1,
+    nextNodes: [
+      { id: 'node-2', videoId: 2 },
+      { id: 'node-3', videoId: 3 },
+      { id: 'node-4', videoId: 4 },
+    ],
+  },
+  'node-16': {
+    videoId: 1,
+    nextNodes: [
+      { id: 'node-2', videoId: 2 },
+      { id: 'node-3', videoId: 3 },
+      { id: 'node-4', videoId: 4 },
+    ],
+  },
+  'node-17': {
+    videoId: 1,
+    nextNodes: [
+      { id: 'node-2', videoId: 2 },
+      { id: 'node-3', videoId: 3 },
+      { id: 'node-4', videoId: 4 },
+    ],
+  },
+  'node-18': {
+    videoId: 1,
+    nextNodes: [
+      { id: 'node-2', videoId: 2 },
+      { id: 'node-3', videoId: 3 },
+      { id: 'node-4', videoId: 4 },
+    ],
+  },
+  'node-19': {
+    videoId: 1,
+    nextNodes: [
+      { id: 'node-2', videoId: 2 },
+      { id: 'node-3', videoId: 3 },
+      { id: 'node-4', videoId: 4 },
+    ],
+  },
+  'node-20': {
+    videoId: 1,
+    nextNodes: [
+      { id: 'node-2', videoId: 2 },
+      { id: 'node-3', videoId: 3 },
+      { id: 'node-4', videoId: 4 },
+    ],
+  },
+  'node-21': {
+    videoId: 1,
+    nextNodes: [
+      { id: 'node-2', videoId: 2 },
+      { id: 'node-3', videoId: 3 },
+      { id: 'node-4', videoId: 4 },
+    ],
+  },
+  'node-22': {
+    videoId: 1,
+    nextNodes: [
+      { id: 'node-2', videoId: 2 },
+      { id: 'node-3', videoId: 3 },
+      { id: 'node-4', videoId: 4 },
+    ],
+  },
+  'node-23': {
+    videoId: 1,
+    nextNodes: [
+      { id: 'node-2', videoId: 2 },
+      { id: 'node-3', videoId: 3 },
+      { id: 'node-4', videoId: 4 },
+    ],
+  },
+}
+
+export const rootNodeId = 'node-1'
