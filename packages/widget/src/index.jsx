6c5f1458ebad3738d41fa84358b0041ae44b2513
Masa <masa373.dev@gmail.com>
2025-12-26 18:17:21 +0900

    fix: video and image path

--- packages/widget/src/index.jsx
+++ packages/widget/src/index.jsx
@@ -82,7 +82,7 @@ function VideoWidget() {
           <img
             ref={thumbnailRef}
             class="circle-thumbnail"
-            src={`${import.meta.env.VITE_BASE_URL}video-thumbnail.png`}
+            src={`${import.meta.env.VITE_BASE_URL}aloop/video-thumbnail.png`}
             alt="Video thumbnail"
           />
           <div class="circle-play-icon">
@@ -93,7 +93,7 @@ function VideoWidget() {
         <div
           class="video-container"
           style={{
-            backgroundImage: `url(${import.meta.env.VITE_BASE_URL}video-background.png)`,
+            backgroundImage: `url(${import.meta.env.VITE_BASE_URL}aloop/video-background.png)`,
             backgroundSize: 'cover',
             backgroundPosition: 'center',
           }}
